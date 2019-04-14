2510b1d70d057c0ab78e86f64600bb7a118dfd6d
MatoPlus <rixin.yang.2001@gmail.com>
2019-04-13 21:16:23 -0400

    moved custom settings to a separate file

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
-
 ;; set up package repositories.
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-
 ;; package management (unless a package is already installed, synchronize with repositories and install package.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -25,48 +23,8 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 ;; load wm
-(org-babel-load-file (expand-file-name "~/.emacs.d/wm.org"))
+;(org-babel-load-file (expand-file-name "~/.emacs.d/wm.org"))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
- '(custom-enabled-themes (quote (dracula)))
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
- '(fancy-splash-image nil)
- '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX" . "#dc752f")
-     ("XXXX" . "#dc752f")
-     ("???" . "#dc752f"))))
- '(package-selected-packages
-   (quote
-    (git-gutter linum-relative undo-tree buffer-move buffer-move\.el neotree projectile magit zzz-to-char flycheck ox-twbs yasnippet-snippets yasnippet sudo-edit expand-region mark-multiple swiper popup-kill-ring hlinum symon dmenu spaceline dashboard rainbow-delimiters company swtich-window smex ido-vertical-mode rainbow-mode avy dracula-theme which-key use-package)))
- '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(linum-highlight-face ((t (:foreground "orchid")))))
+;; load looks and feels
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
